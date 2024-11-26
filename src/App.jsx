6de103538c5cc86002599949ba191d841101f141
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2024-11-26 21:09:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import Sectionone from './Components/Section_One/Sectionone'
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Sectionone/>
     </div>
   )
 }
