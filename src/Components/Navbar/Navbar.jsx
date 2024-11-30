9e2d063455bbaea3f242a8e3a0ba064fe300702b
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2024-12-01 00:09:55 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
     <nav className='nav'>
         <img src={logo} alt='' className='logo'/>
         <ul>
-            <li>Auctions</li>
-            <li>Sell a vehicle
+            <li>Auctions
                 <ul className='dropdown'>
                     <li>Live Auction</li>
                     <li>Featured Auction</li>
                     <li>Past Result</li>
                 </ul>
             </li>
+            <li>Sell a vehicle</li>
             <li>Help Center</li>
             <li>News</li>
         </ul>
