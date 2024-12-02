ac3284266426acb4744c44f0b53ed3ad8e6cf738
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2024-12-03 00:24:48 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Auction from './Components/Auction/Auction'
 
 const App = () => {
   return (
     <div className="container">
       <Navbar/>
       <Hero/>
+      <Auction/>
     </div>
   )
 }
---
+++ src/Components/Auction/Auction.jsx
@@ -0,0 +1,188 @@
+import React from 'react'
+import './Auction.css'
+import img_1 from '../../assets/1.jpg';
+import img_2 from '../../assets/2.jpg';
+import img_3 from '../../assets/3.jpg';
+import img_4 from '../../assets/4.jpg';
+import img_5 from '../../assets/5.jpg';
+
+const Auction = () => {
+  return (
+      <div className="card-container">
+
+
+
+
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        
+
+
+
+
+
+
+
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+        <div className="card">
+            <img src={img_1} alt=''/>
+            <div className="card-content">
+                <h3>Nissan juke 2010</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisi</p>
+                <button type="button" className='btn'>Submit Bid</button>
+            </div>
+        </div>
+      </div>
+  )
+}
+
+export default Auction
