3a3c12c022159f548cc86210eb8a5f9fa8d6e63c
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2024-12-17 23:16:47 +0530

    .

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
+    <div className='abt-container'>
       <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
+      <p>
+        Over the last few years, many car enthusiasts have started turning their attention to recent vehicles – cars from the 1980s, 1990s, and beyond. Automotive reviewer Doug DeMuro realized there isn’t yet a specific place that’s focused solely on buying and selling these modern enthusiast cars, but there should be – so he and a team created Cars & Bids, with its simple name modeled after Doug’s famous pursuit of “quirks and features.”
+      Cars & Bids is the best online auction marketplace to buy and sell modern enthusiast cars – and that means pretty much anything that’s cool from the 1980s, 1990s, 2000s, 2010s, or 2020s. To us, “cool” ranges from the obvious (a Ferrari F355 or a Lamborghini Gallardo) to the esoteric (a pristine Dodge Dakota Convertible or a Mercury Capri XR2) to the traditional fun cars that enthusiasts love (a Mazda MX-5 Miata or a Porsche 911). Basically everything that’s exciting, fun, interesting, or quirky is welcome here – as long as it comes from the modern era.
+      </p>
     </div>
   )
 }
