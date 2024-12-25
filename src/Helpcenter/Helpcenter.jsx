8c71b5ff5f11d66789668109094bd3705757ffd2
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2024-12-25 11:42:12 +0530

    create news page

--- src/Helpcenter/Helpcenter.jsx
+++ src/Helpcenter/Helpcenter.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import './Helpcenter.css'
 
 const Helpcenter = () => {
   return (
-    <div>
-      
+    <>
+    <div className='title'>
+                <h2>Help Center</h2>
+            </div>
+
+    <div className="ap-container">
+        <div className="form-container">
+        <form className="form">
+            <div className="form-group">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" required=""/>
+            </div>
+            <div className="form-group">
+            <label for="textarea">How Can We Help You?</label>
+            <textarea name="textarea" id="textarea" rows="10" cols="50" required="">          </textarea>
+            </div>
+            <button className="form-submit-btn" type="submit">Submit</button>
+        </form>
+        </div>
     </div>
+    </>
   )
 }
 
