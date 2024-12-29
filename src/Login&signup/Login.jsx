69a9c8d393b398b2e5235f2f183fcb477f9d615b
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2024-12-29 23:56:51 +0530

    Done sign pages

--- src/Login&signup/Login.jsx
+++ src/Login&signup/Login.jsx
@@ -3,7 +3,24 @@ import './Login&signup.css'
 
 const Login = () => {
   return (
-    <></>
+    <>
+      <div className="sign-container">
+      <div class="form-box-sign">
+        <form class="form-sign">
+            <span class="title-sign">Sign in</span>
+            <span class="subtitle">Log in to your account.</span>
+            <div class="form-container-sign">
+              <input type="email" class="input" placeholder="Email"/>
+              <input type="password" class="input" placeholder="Password"/>
+            </div>
+            <button>Sign in</button>
+        </form>
+        <div class="form-section">
+          <p>No account? <a href="">Sign up</a> </p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
--- src/Login&signup/Signup.jsx
+++ src/Login&signup/Signup.jsx
@@ -12,11 +12,12 @@ const Signup = () => {
               <input type="text" class="input" placeholder="Full Name"/>
               <input type="email" class="input" placeholder="Email"/>
               <input type="password" class="input" placeholder="Password"/>
+              <input type="password" class="input" placeholder="Confirm Password"/>
             </div>
             <button>Sign up</button>
         </form>
         <div class="form-section">
-          <p>Have an account? <a href="">Log in</a> </p>
+          <p>Have an account? <a href="">Sign in</a> </p>
         </div>
       </div>
     </div>
